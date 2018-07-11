9930370e38ddd7441d2e920cbb0db32be7ef1388
TessACraig89 <tesscraig.webdev@gmail.com>
2018-07-10 21:51:27 -0500

    adds eventListener syntax psuedocode

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const player2 = document.getElementById("player2");
 let player1Pos = 0;
 let player2Pos = 0;
 
-
+// target.addEventListener(type, listener[, options]);
 document.addEventListener('keypress', (event) => {
+
   if (event.key === "l") {
     player1Pos = player1Pos + 25;
     player1.setAttribute("style", "left: " + player1Pos + "px");
@@ -14,8 +15,3 @@ document.addEventListener('keypress', (event) => {
     player2.setAttribute("style", "left: " + player2Pos + "px");
   }
 });
-
-// document.addEventListener('keypress', function(k) {
-//     player2Pos = player2Pos + 25;
-//     player2.setAttribute("style", "left: " + player2Pos + "px");
-// });
