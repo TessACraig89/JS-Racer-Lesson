18656532a6bcade41fae4b0765e4093c47e0d520
TessACraig89 <tesscraig.webdev@gmail.com>
2018-07-10 21:35:38 -0500

    player1 keypress event moves player properly

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const player1 = document.getElementById("player1");
 const player2 = document.getElementById("player2");
 
-document.addEventListener('keydown', function(l) {
-    player1.style.left = "50px";
+let player1Pos = 0;
+let player2Pos = 0;
+
+
+document.addEventListener('keypress', function(l) {
+    player1Pos = player1Pos + 25;
+    player1.setAttribute("style", "left: " + player1Pos + "px");
 });
