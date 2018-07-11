4d97a9e83113a7571a2d9233d97efe3358b787f5
TessACraig89 <tesscraig.webdev@gmail.com>
2018-07-10 21:48:33 -0500

    changes keypress event so that player elements move seperately

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ let player1Pos = 0;
 let player2Pos = 0;
 
 
-document.addEventListener('keypress', function(l) {
+document.addEventListener('keypress', (event) => {
+  if (event.key === "l") {
     player1Pos = player1Pos + 25;
     player1.setAttribute("style", "left: " + player1Pos + "px");
+  } else if (event.key === "k") {
+    player2Pos = player2Pos + 25;
+    player2.setAttribute("style", "left: " + player2Pos + "px");
+  }
 });
+
+// document.addEventListener('keypress', function(k) {
+//     player2Pos = player2Pos + 25;
+//     player2.setAttribute("style", "left: " + player2Pos + "px");
+// });
