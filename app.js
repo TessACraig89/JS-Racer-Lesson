ce3b2a33add796a47d7f5322eeecc817c561b927
TessACraig89 <tesscraig.webdev@gmail.com>
2018-07-10 21:14:36 -0500

    WIP keydown event

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 const player1 = document.getElementById("player1");
 const player2 = document.getElementById("player2");
+
+document.addEventListener('keydown', function(l) {
+    player1.style.left = "50px";
+});
